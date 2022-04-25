1d21d0dba6b5289401abe0572753e8f524ba5b10
emmanuel.rangel <emmanuel.rangel@tecreemos.com>
2022-04-25 13:23:20 -0500

    - Se agrega funcionalidad de huella digital

--- permissions_demo_app/android/app/src/main/kotlin/mx/techreo/permissions_demo_app/MainActivity.kt
+++ permissions_demo_app/android/app/src/main/kotlin/mx/techreo/permissions_demo_app/MainActivity.kt
@@ -1,6 +1,13 @@
 package mx.techreo.permissions_demo_app
 
-import io.flutter.embedding.android.FlutterActivity
+import androidx.annotation.NonNull
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine:
+                                        FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
+    }
 }
--- permissions_demo_app/lib/biometrics.dart
+++ permissions_demo_app/lib/biometrics.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:local_auth/local_auth.dart';
 
-class Biometrics extends StatelessWidget {
+class Biometrics extends StatefulWidget {
   const Biometrics({Key? key}) : super(key: key);
 
   @override
+  _BiometricsState createState() => _BiometricsState();
+}
+  /*@override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -17,6 +21,73 @@ class Biometrics extends StatelessWidget {
           child: const Text('Go back!'),
         ),
       ),
+    );
+  }*/
+
+class _BiometricsState extends State<Biometrics> {
+  late LocalAuthentication _localAth;
+  bool _isBiometricAvailable = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _localAth = LocalAuthentication();
+    _localAth.canCheckBiometrics.then((b) {
+      setState(() {
+        _isBiometricAvailable = b;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Location"),
+        ),
+        body: Center(
+          child: Directionality(
+            textDirection: TextDirection.rtl,
+            child: ElevatedButton.icon(
+              icon: const Icon(Icons.fingerprint),
+              label: const Text("Verificar huella digital"),
+              onPressed: () async {
+                if(_isBiometricAvailable) {
+                  bool didAuthenticate = await _localAth
+                      .authenticateWithBiometrics(
+                      localizedReason: "Iniciar sesion con huella");
+                  if (didAuthenticate) {
+                    mostrarAlerta(context, "Autenticación correcta");
+                  } else {
+                    mostrarAlerta(context, "Autenticación incorrecta");
+                  }
+                }else{
+                  mostrarAlerta(context, "el dispositivo no cuenta con esta función");
+                }
+              },
+              //icon: const Icon( Icons.fingerprint,),
+            ),
+          ),
+        ),
+    );
+  }
+
+  void mostrarAlerta(BuildContext context, String texto){
+    showDialog(
+      barrierDismissible: false,
+      context: context,
+      builder: (BuildContext context){
+        return AlertDialog(
+          title: const Text("Autenticación"),
+          content: Text(texto),
+          actions: <Widget>[
+            TextButton(onPressed: (){
+              Navigator.pop(context);
+            }, child: const Text("OK")
+            )
+          ],
+        );
+      },
     );
   }
 }
